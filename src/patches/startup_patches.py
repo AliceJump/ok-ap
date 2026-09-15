d8bf204fe65d4e92a13d0dbc3aab8279952667a8
AliceJump <149395013+AliceJump@users.noreply.github.com>
2026-09-16 00:27:41 +0800

    refactor: remove schedule task name patch
    Remove the schedule task name patch that converted -t index arguments
    to task names for Windows scheduled tasks. This patch is no longer
    needed as the scheduling mechanism has been updated.

--- src/patches/startup_patches.py
+++ src/patches/startup_patches.py
@@ -11,14 +11,12 @@ def install_startup_patches():
     from src.patches.i18n_collection_patch import install_i18n_collection_patch
     from src.patches.ocr_text_fix_patch import install_ocr_text_fix_patch
     from src.patches.screenshot_sidecar_patch import install_screenshot_sidecar_patch
-    from src.patches.schedule_task_name_patch import install_schedule_task_name_patch
     from src.patches.startup_window_patch import install_startup_window_patch
     from src.patches.task_config_lock_patch import install_task_config_lock_patch
 
     install_i18n_collection_patch()
     install_ocr_text_fix_patch()
     install_screenshot_sidecar_patch()
-    install_schedule_task_name_patch()
     install_startup_window_patch()
     install_task_config_lock_patch()
     _PATCH_INSTALLED = True
